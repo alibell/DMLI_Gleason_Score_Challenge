c1a70e6e626810ba87fd149601d6787aef8d1016
Ali <contact@alibellamine.me>
2022-04-03 00:26:24 +0200

    Adding utils

--- model1/utils/__init__.py
+++ model1/utils/__init__.py
@@ -1,10 +1,15 @@
+from numpy import bincount
 import pandas as pd
 
-def dataframe_from_predictions (prediction, main_df):
+def dataframe_from_predictions (prediction, main_df, bincount=True):
     prediction_df = pd.DataFrame(prediction).T \
         .reset_index() \
         .assign(index = lambda x: x["index"].apply(lambda y: y.split(".")[0])) \
         .rename(columns = {"index":"image_id"})
+
+    if bincount:
+        for column in ["gleason1", "gleason2"]:
+            prediction_df[column] = prediction_df[column].apply(lambda x: np.bincount(x, minlength=6))
 
     output_df = pd.merge(
         prediction_df,

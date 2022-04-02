fa48f28d90412f54a2e56aeb15bb0d873dcecdf8
Ali <contact@alibellamine.me>
2022-04-03 00:19:13 +0200

    Utils functions

---
+++ model1/utils/__init__.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def dataframe_from_predictions (prediction, main_df):
+    prediction_df = pd.DataFrame(prediction).T \
+        .reset_index() \
+        .assign(index = lambda x: x["index"].apply(lambda y: y.split(".")[0])) \
+        .rename(columns = {"index":"image_id"})
+
+    output_df = pd.merge(
+        prediction_df,
+        main_df,
+        left_on="image_id",
+        right_on="image_id",
+        how="inner"
+    )
+
+    return output_df

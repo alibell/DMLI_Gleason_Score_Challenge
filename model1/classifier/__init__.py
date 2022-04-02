7804de03a2017614c2d9d5013ce48a8d1fbfed1e
Ali <contact@alibellamine.me>
2022-04-03 00:46:02 +0200

    Adding isup rules

--- model1/classifier/__init__.py
+++ model1/classifier/__init__.py
@@ -4,6 +4,49 @@
 
 from sklearn.ensemble import RandomForestClassifier
 
+isup_rules = {
+    0:{
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0
+    },
+    1:{
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0
+    },
+    3:{
+        0:1,
+        1:1,
+        2:1,
+        3:1,
+        4:2,
+        5:4
+    },
+    4:{
+        0:3,
+        1:3,
+        2:3,
+        3:3,
+        4:4,
+        5:5
+    },
+    5:{
+        0:4,
+        1:4,
+        2:4,
+        3:4,
+        4:5,
+        5:5
+    }
+}
+
 class imageClassifier ():
     def __init__ (self):
         self.model = RandomForestClassifier(n_estimators=200)

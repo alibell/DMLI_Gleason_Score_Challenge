3b931dffb556fe634c12a12e362f0a9db2f5e8a6
Ali BELLAMINE <contact@alibellamine.me>
2022-04-03 21:03:35 +0200

    Adding tiles classifier

--- model2/models/__init__.py
+++ model2/models/__init__.py
@@ -62,3 +62,53 @@ class patchClassifier (nn.Module):
     loss_ = loss.detach().cpu().item()
 
     return loss_
+
+class tilesClassifier (nn.Module):
+  def __init__ (self):
+    super().__init__()
+
+    self.backbone = efficientnet_b0(pretrained=True)
+    classifier_input = self.backbone.classifier[1].in_features
+    self.backbone.classifier[1] = nn.Sequential(
+        nn.Linear(classifier_input, 6)
+    )
+
+    for parameter in self.backbone.parameters():
+      parameter.requires_grad = True
+
+    self.softmax = nn.Softmax(dim=0)
+
+    self.network = self.backbone
+
+    self.criterion = nn.CrossEntropyLoss()
+    self.optim = optim.AdamW(self.parameters())
+
+  def forward(self, x):
+    predictions = self.network(x)
+    y_hat = predictions
+
+    return y_hat
+
+  def predict(self, x):
+    self.eval()
+
+    with torch.no_grad():
+      y_hat = self.forward(x)
+      y_hat = self.softmax(y_hat)
+
+      return y_hat
+
+  def fit(self, x, y):
+    self.train()
+    self.optim.zero_grad()
+    
+    y_hat = self.forward(x)
+    loss = self.criterion(y_hat, y)
+
+    loss.backward()
+
+    self.optim.step()
+
+    loss_ = loss.detach().cpu().item()
+
+    return loss_
